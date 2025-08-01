a13235b34f149b569dc59e357b31b0a6beb713c3
Maq4 <juanacorreoparaclases@gmail.com>
2025-07-31 23:17:23 -0400

    Update app.js agregada funcion mostrar alerta

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function mostrarMensajeEnLaConsola() {
     console.log('El botón fue clicado!')
 }
+function mostrarAlerta() {
+    let ciudad = prompt("Por favor, ingresa el nombre de una ciudad de Brasil:");
+    alert("Estuve en " + ciudad + " y me acordé de ti.");
+}

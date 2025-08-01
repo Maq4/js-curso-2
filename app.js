2d26a0133668e0ad412b5d8477aed5d00dd328b9
Maq4 <juanacorreoparaclases@gmail.com>
2025-07-31 23:23:18 -0400

    Update app.js funcion suma de dos numeros

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ function mostrarMensaje() {
 function mostrarAlerta(){
     alert("AMO JS");
 } 
+function sumaDosNumeros(){
+    let primerNumero = parseInt(prompt('Digite el primer numero'));
+    let segunNumero = parseInt(prompt('Digite el segundo numero'));
+    let resultado = primerNumero + segunNumero;
+    alert('${primerNumero} + ${segunNumero} = ${resultado}')
+}
